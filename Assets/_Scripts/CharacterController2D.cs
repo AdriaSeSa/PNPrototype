17020fa969fd5a2a4b203b802125bce1f9441cad
AdriaSeSa <76749150+AdriaSeSa@users.noreply.github.com>
2021-09-18 23:46:04 +0200

    Add Player Basic Movement
    Player lateral movement, low jump and high jump, input buffer on PlayerMovement2D.

---
+++ Assets/_Scripts/CharacterController2D.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+    [SerializeField] private float fallMultiplier;								// Gravity multiplier to fall faster
+    [SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck1, m_GroundCheck2;			// A position marking where to check if the player is grounded.
+
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    public bool m_Grounded;            // Whether or not the player is grounded.
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    private Vector3 m_Velocity = Vector3.zero;
+    private bool isFalling = false;
+
+    
+    //TODO:Change Unity Events for normal events / boolean events
+    public UnityEvent OnJumpEvent;		//Calls when jumping
+    public UnityEvent OnLandEvent;		// Calls when landing on floor
+    public UnityEvent OnRunEvent;		//Calls when running
+    public UnityEvent OnStopEvent;		// Calls when Stopping running
+    
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+        if (OnRunEvent == null)
+            OnRunEvent = new UnityEvent();
+        if (OnStopEvent == null)
+            OnStopEvent = new UnityEvent();
+        if (OnJumpEvent == null)
+            OnJumpEvent = new UnityEvent();
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck positions hits anything designated as ground
+
+        List<Collider2D> colliders = new List<Collider2D>();
+        
+        colliders.AddRange(Physics2D.OverlapCircleAll(m_GroundCheck1.position, k_GroundedRadius, m_WhatIsGround)
+            .ToList());
+        colliders.AddRange(Physics2D.OverlapCircleAll(m_GroundCheck2.position, k_GroundedRadius, m_WhatIsGround)
+            .ToList());
+        
+        for (int i = 0; i < colliders.Count; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+                if (!wasGrounded)
+                {
+                    OnLandEvent.Invoke();
+                }
+				
+            }
+        }
+    }
+
+    public void Move(float moveDir, bool jump)
+    {
+        // We can only control the player if it is on ground or air control is activated
+        if (m_Grounded || m_AirControl)
+        {
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(moveDir * 10f, m_Rigidbody2D.velocity.y);
+
+            // And then smoothing it out and applying it to the character
+            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity,
+                m_MovementSmoothing);
+
+
+            // If the input is moving the player right and the player is facing left...
+            if (moveDir > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (moveDir < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+
+        // If player is not grounded
+        if (!m_Grounded)
+        {
+            // We check if the player is falling by checking its vertical velocity. If it is less than -0.5, the player is falling.
+            isFalling = m_Rigidbody2D.velocity.y < -0.5; 
+        
+            // If player released the jump button and we are not falling yet, we want to start falling
+            if (m_Rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump")) 
+            {
+                m_Rigidbody2D.velocity += Vector2.up * (Physics2D.gravity.y * (fallMultiplier * Time.deltaTime));
+            }
+        }
+  
+
+        
+        // If the player should jump...
+        if (jump)
+        {
+            OnJumpEvent.Invoke(); // We call Jumping event
+
+            // If we are grounded we can jump
+            if (m_Grounded)
+            {
+                // Add a vertical force to the player.
+                m_Grounded = false;
+                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce), ForceMode2D.Impulse);
+            }
+        }
+        
+        // If we are falling, we want to add a force to make us fall faster.
+        if (isFalling)
+        {
+            m_Rigidbody2D.velocity += Vector2.up * (Physics2D.gravity.y * (fallMultiplier * Time.deltaTime));
+        }
+    }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/_Scripts/PlayerMovement2D.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script uses the CharacterController2D 
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    private CharacterController2D _characterController;
+
+    public float runSpeed = 40f;
+    private bool _jump = false;
+
+    private float _horizontalInput;
+
+    private Queue<KeyCode> _inputBuffer = new Queue<KeyCode>();
+    [SerializeField]
+    private float _inputDequeueTime;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Basic Movement Input
+        
+        _horizontalInput = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _inputBuffer.Enqueue(KeyCode.Space);
+            Invoke("DequeueAction", _inputDequeueTime);
+        }
+
+        if (_inputBuffer.Count > 0)
+        {
+            if (_inputBuffer.Peek() == KeyCode.Space && _characterController.m_Grounded)
+            {
+                _inputBuffer.Dequeue();
+                _jump = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _characterController.Move(_horizontalInput * Time.fixedDeltaTime, _jump);
+        _jump = false;
+    }
+
+    private void DequeueAction()
+    {
+        if (_inputBuffer.Count > 0) _inputBuffer.Dequeue();
+    }
+}
